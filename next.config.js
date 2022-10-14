60b2bc675f9738e05d76ab7448b0cb98f4aa9208
Linathi <mqalolinathi@gmail.com>
2022-10-14 20:18:57 +0200

    initial commit

--- next.config.js
+++ next.config.js
@@ -4,4 +4,10 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  images: {
+    domains: ["links.papareact.com", "image.tmdb.org"],
+  },
+};
+
